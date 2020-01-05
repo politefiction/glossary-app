17ded6123bcbe0084555056685935c3b2f240424
Chelsi Jones <cajones.bz@gmail.com>
2020-01-04 18:31:17 -0600

    Momentarily disable production code in server package.json

--- server/index.js
+++ server/index.js
@@ -31,13 +31,13 @@ require('./validation/passport')
 app.use('/api', entryRouter)
 app.use('/api/users', userRouter)
 
-/*
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../client/build'))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
   })
 }
-*/
+
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
