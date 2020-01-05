ce03dc418ddd57d523e59efa2b7bdf3f9e695fb2
Chelsi Jones <cajones.bz@gmail.com>
2020-01-05 10:17:16 -0600

    Rearrange server index file in attempt to display client

--- index.js
+++ index.js
@@ -10,27 +10,16 @@ const userRouter = require('./routes/user-router')
 const app = express()
 const apiPort = process.env.PORT || 8000
 
-var corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true
-}
-
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors(corsOptions))
 app.use(express.json())
 
 glossaryDB.on('error', console.error.bind(console, 'MongoDB connection error:'))
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.use(passport.initialize())
 require('./validation/passport')
 
 app.use('/api', entryRouter)
 app.use('/api/users', userRouter)
-
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
@@ -39,5 +28,11 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true
+}
+
+app.use(cors(corsOptions))
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
